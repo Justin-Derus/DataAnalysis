ae25ed1cf55c1528b0b80344afa086c7f147e8b9
Justin Derus <scoeyderus@gmail.com>
2026-01-25 17:12:00 -0600

    updates

---
+++ PowerBI/PremiumDogOutlet/PremiumDogOutletsSQL/Top3SalesmenPerBranchAllTime.sql
@@ -0,0 +1,19 @@
+-- Find The Top 3 Salesmen From Each Branch Uses Window Fxn, Join, Case When - Ties are included (rank() function)
+WITH RankedSales AS ( -- Use a CTE to get all of the information pertaining to the sales and employee number
+    SELECT 
+    	SUM(ti.SaleTotal) as SalesAmount, t.EmployeeID AS EmpID, t.BranchID as Branch,
+    	DENSE_RANK() OVER(PARTITION BY t.BranchID ORDER BY SUM(ti.SaleTotal) DESC) AS SaleRank -- window fxn for getting top 3 per branch
+    FROM TRANSACTIONS AS t
+    JOIN transactionitems as ti on ti.TransactionID = t.TransactionID WHERE t.BranchID <> 0 AND t.EmployeeID IS NOT NULL -- branch 0 means all combined, null emp is anon transaction
+    GROUP BY t.BranchID, t.EmployeeID
+)
+SELECT CONCAT('$', FORMAT(rs.SalesAmount,2)) as TotalSales, rs.Branch, rs.EmpID, e.EmployeeName,
+CASE
+    WHEN rs.SaleRank = 1 THEN "First"
+    WHEN rs.SaleRank = 2 THEN "Second"
+    WHEN rs.SaleRank = 3 THEn "Third"
+END AS SalesRanking
+FROM RankedSales as rs
+JOIN employee as e on e.EmployeeID = rs.EmpID
+WHERE SaleRank <= 3
+ORDER BY Branch, SaleRank ASC;
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # DataAnalysis
 Projects Pertaining to Data Analysis 
+
+Online Retail - 2011 Dataset
+![Sales Dashboard](Excel/eCommerceExcelDash.png)
+
+Premium Dog Outlets
+![Branch Sales](PowerBI/PremiumDogOutlet/Branch%20Sales.png)
+![Highest Selling Months](PowerBI/PremiumDogOutlet/Highest%20Selling%20Months.png)
+![Yearly Profits](PowerBI/PremiumDogOutlet/Yearly%20Profits.png)
+
+Online Retail
+![Recurring Customer Heat Map](PowerBI/OnlineRetail/Recurring%20Customer%20Heat%20Map.png)
+
+Houseware Retail
+![Sale Trends](PowerBI/houseware/Sale%20Trends.png)
+![Sales By State](PowerBI/houseware/Sales%20By%20State.png)
+![Units Sold vs Sale Amount](PowerBI/houseware/Units%20Sold%20vs%20Sale%20Amount.png)
+![Units Soldt](PowerBI/houseware/Units%20Sold.png)
